de60bbe58c3d6cfb14b9fdae6817f32b0731426f
Sorin Neacsu <sorin@users.noreply.github.com>
2021-09-05 21:15:14 -0700

    Create secrets.py

---
+++ PM25/secrets.py
@@ -0,0 +1,10 @@
+secrets = {
+    'ssid' : '...',
+    'password' : '...',
+    'aio_user' : '...',
+    'aio_key' : '...',
+    'timezone' : "America/Los_Angeles",
+    'latitude': 0.0,
+    'longitude': 0.0,
+    'elevation': 0
+}
